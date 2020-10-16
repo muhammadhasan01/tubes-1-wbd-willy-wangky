27827fd3764d4e5a9f98a4151f8095a03759a3a7
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-10-16 17:38:32 +0700

    Add basic product-card component

---
+++ components/card/product-card.php
@@ -0,0 +1,8 @@
+<style><?php include 'product-card.css'?></style>
+    <!-- TODO: Make dynamic !-->
+    <div class="card">
+    <img src='../../public/images/choco-1.jpg' alt="Denim Jeans" style="width:100%">
+    <h1>Cadburry</h1>
+    <p class="amount">Amount sold: 6</p>
+    <p class="price">Price: Rp. 200.000,00 </p>
+</div>
