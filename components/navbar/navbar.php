5b9eaecbec3866aa7bd7f6954368a3c1c6fac568
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-10-16 17:04:10 +0700

    Add navbar basics

---
+++ components/navbar/navbar.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="navbar.css">
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <!-- TODO: Handle User and Superuser -->
+    <a href="#news">History</a>
+    <input type="text" placeholder="Search">
+    <div class="topnav-right">
+        <a href="#logout">Logout</a>    
+    </div>
+</div>
