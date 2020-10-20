f006915618b2dc5cc58f1e46d673ce2cee605697
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-10-20 21:09:28 +0700

    Add search-result basics

---
+++ components/card/search-result-card.php
@@ -0,0 +1,13 @@
+<style><?php include 'search-result-card.css';?></style>
+<!-- TODO: Make dynamic + link to buy -->
+<div class="card">
+    <img src="../../public/images/choco-1.jpg"></img>
+    <div class="details">
+        <p class="title">Choco Name 1</p>
+        <p class="price">Price: Rp 3.000,00</p>
+        <p class="amount">Amount remaining: 15</p>
+        <p class="desc">
+            Description:<br/>Coklat ini merupakan coklat yang dipanggang meleleh, setelah itu dibentuk lalu dibekukan.
+        </p>
+    </div>
+</div>
---
+++ src/search-result/search-result.php
@@ -0,0 +1,9 @@
+<?php
+    include('../../components/navbar/navbar.php');
+?>
+
+<!-- Get Search Results -->
+
+<?php
+    include('../../components/card/search-result-card.php');
+?>
