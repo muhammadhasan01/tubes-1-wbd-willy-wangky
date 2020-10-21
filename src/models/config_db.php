b03d5ffc7636f3e25e814ccffb50ce2a28185521
fabianzhafran <13518022@std.stei.itb.ac.id>
2020-10-21 20:04:54 +0700

    Fix cookie checker user require

--- src/models/config_db.php
+++ src/models/config_db.php
@@ -1,15 +1,15 @@
 <?php
 
 // localhost
-define('DB_SERVER', '127.0.0.1'); //
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'rootroot');
-define('DB_DATABASE', 'willy_wangky');
-
-// define('DB_SERVER', 'db4free.net'); //
-// define('DB_USERNAME', 'root_wangky');
+// define('DB_SERVER', '127.0.0.1'); //
+// define('DB_USERNAME', 'root');
 // define('DB_PASSWORD', 'rootroot');
 // define('DB_DATABASE', 'willy_wangky');
+
+define('DB_SERVER', 'db4free.net'); //
+define('DB_USERNAME', 'root_wangky');
+define('DB_PASSWORD', 'rootroot');
+define('DB_DATABASE', 'willy_wangky');
 
 
 ?>
