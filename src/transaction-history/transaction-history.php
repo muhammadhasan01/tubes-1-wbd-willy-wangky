22800a2e1ba08d201809962b620555c76a397eda
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-10-23 19:52:52 +0700

    Add transaction history structure

---
+++ src/transaction-history/transaction-history.php
@@ -0,0 +1,36 @@
+<?php
+    include('../../components/navbar/navbar.php');
+?>
+
+<style><?php include 'transaction-history.css'?></style>
+<div class="container">
+<div class="title">Transaction History</div>
+<!-- TODO: Handle when there is no transaction yet (optional) -->
+<table class="transaction-history">
+<tr>
+    <th>Chocolate Name</th>
+    <th>Amount</th>
+    <th>Total Price</th>
+    <th>Date</th>
+    <th>Time</th>
+    <th>Address</th>
+</tr>
+<!-- TODO: Fill with data transiction !-->
+<tr>
+    <td>Choco Name 1</td>
+    <td>7</td>
+    <td>Rp 21.000,00</td>
+    <td>7 Oktober 2020</td>
+    <td>09:20:21</td>
+    <td>Indonesia</td>
+</tr>
+<tr>
+    <td>Choco Name 1</td>
+    <td>7</td>
+    <td>Rp 21.000,00</td>
+    <td>7 Oktober 2020</td>
+    <td>09:20:21</td>
+    <td>Indonesia</td>
+</tr>
+</table>
+</div>
